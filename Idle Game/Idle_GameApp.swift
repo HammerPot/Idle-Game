6be4fe81c3078c546561aa07d4309bc4f593a56e
HammerPot <nailington@mathstudy.dev>
2025-06-24 17:27:39 -0400

    Initial Commit

---
+++ Idle Game/Idle_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Idle_GameApp.swift
+//  Idle Game
+//
+//  Created by Potter on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Idle_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Idle GameTests/Idle_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Idle_GameTests.swift
+//  Idle GameTests
+//
+//  Created by Potter on 6/24/25.
+//
+
+import Testing
+@testable import Idle_Game
+
+struct Idle_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
